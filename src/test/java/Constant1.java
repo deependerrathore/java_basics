a88b069140f75e187aa4692349df931e0484c2c4
Deepender Rathore <deepenrathore28@gmail.com>
2019-04-03 23:32:10 +1100

    Constant 1

---
+++ src/test/java/Constant1.java
@@ -0,0 +1,11 @@
+package test.java;
+
+public class Constant1 {
+	static final int NUMBER_OF_MONTHS = 12;
+	static final double PI = (double)22/7;
+	
+	public static void main(String[] args) {
+		System.out.println("NUMBER_OF_MONTHS: " + NUMBER_OF_MONTHS);
+		System.out.println("PI: " + PI);
+	}
+}
