46c864dfcf6749b82c85d1759bf8393aea3393cf
Deepender Rathore <deepenrathore28@gmail.com>
2019-04-03 23:23:56 +1100

    Boolean Demo

---
+++ src/test/java/BoolTest.java
@@ -0,0 +1,17 @@
+package test.java;
+
+public class BoolTest {
+	public static void main(String[] args) {
+		boolean b;
+		
+		b= false;
+		System.out.println("b is " + b);
+		
+		b = true;
+		System.out.println("b is " + b);
+		
+		if (b) {
+			System.out.println("if is executed");
+		}
+	}
+}
