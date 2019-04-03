4a27bf1ca1096a43457a54864c4a27392a3177bd
Deepender Rathore <deepenrathore28@gmail.com>
2019-04-03 23:34:30 +1100

    public constant

--- src/test/java/Constant1.java
+++ src/test/java/Constant1.java
@@ -1,11 +1,11 @@
 package test.java;
 
 public class Constant1 {
-	static final int NUMBER_OF_MONTHS = 12;
-	static final double PI = (double)22/7;
+	public static final int NUMBER_OF_MONTHS = 12;
+	public static final double PI = (double)22/7;
 	
 	public static void main(String[] args) {
-		System.out.println("NUMBER_OF_MONTHS: " + NUMBER_OF_MONTHS);
-		System.out.println("PI: " + PI);
+		System.out.println("NUMBER_OF_MONTHS: " + Constant1.NUMBER_OF_MONTHS); // we can access the constant without class name as well
+		System.out.println("PI: " + Constant1.PI);
 	}
 }
