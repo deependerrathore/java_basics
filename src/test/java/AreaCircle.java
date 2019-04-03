769c56f9a9d4fcae49bcbdc918b04af80451fb5a
Deepender Rathore <deepenrathore28@gmail.com>
2019-04-03 22:13:06 +1100

    Area of the circle

---
+++ src/test/java/AreaCircle.java
@@ -0,0 +1,13 @@
+package test.java;
+
+public class AreaCircle {
+	
+	public static void main(String[] args) {
+		double rad;
+		final double PI = 3.14159;
+		rad = 10.0;
+		double area = PI * rad * rad;
+		System.out.println("Area of the circle is = " + area);
+	}
+
+}
