62ae73f28350eba93bd0ce1bc3fc646fddeffcdc
Deepender Rathore <deepenrathore28@gmail.com>
2019-04-03 23:21:02 +1100

    Char example

---
+++ src/test/java/CharDemo.java
@@ -0,0 +1,14 @@
+package test.java;
+
+public class CharDemo {
+	public static void main(String[] args) {
+		char aChar ,bChar;
+		aChar = 88;
+		bChar = 'Y';
+		System.out.println("aChar and bChar: ");
+		System.out.println(aChar + " " + bChar);
+		aChar++;//Increment aChar
+		System.out.println(aChar + " " + bChar);
+		
+	}
+}
